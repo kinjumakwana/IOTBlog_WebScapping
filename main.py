b321b0857bc36a42830bda597118e05292d47c6f
Kinjal Makwana <109803739+kinjumakwana@users.noreply.github.com>
2023-04-19 18:39:24 +0530

    site all data into csv

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class IOTblog:
         try:
             self.driver.maximize_window()
             self.driver.get("https://blogs.cisco.com/internet-of-things")
-            sleep(10)
+            sleep(5)
             
             ifpagination = True
             blogs_data = []
@@ -38,7 +38,7 @@ class IOTblog:
                 # sleep(10)
                 data = self.driver.find_elements(By.CLASS_NAME,"blog-card")
                 totalblogonpage = len(data)
-                print("totalblogonpage: ",totalblogonpage)
+                # print("totalblogonpage: ",totalblogonpage)
                 
                 # for data in data:
                 #     print(data.text)#first page all blog data
@@ -46,7 +46,7 @@ class IOTblog:
 
                 cardlinks = self.driver.find_elements(By.CLASS_NAME,"card-link")
                 totalbloglink = len(cardlinks)
-                print("totalbloglink: ",totalbloglink)
+                # print("totalbloglink: ",totalbloglink)
                 
                 # for cardlink in cardlinks:
                 for i in range(totalbloglink):
@@ -65,56 +65,56 @@ class IOTblog:
                     
                     Blog_dict = {}
                     title = self.driver.find_element(By.CLASS_NAME,"entry-title")
-                    print("Title: ",title.text)
+                    # print("Title: ",title.text)
                     Blog_dict["Title"] = title.text
                     
                     author = self.driver.find_element(By.XPATH,"/html/body/cdc-template-micro/div/div[1]/div/main/article/div/div[1]/div[2]/p/a")
-                    print("Author: ",author.text)
+                    # print("Author: ",author.text)
                     Blog_dict["Author"] = author.text
                     
                     description = self.driver.find_element(By.CLASS_NAME,"entry-content")
-                    print("Description: ",description.text.encode('utf8'))
+                    # print("Description: ",description.text.encode('utf8'))
                     Blog_dict["Description"] = description.text.encode('utf8')
                     
                     tags = self.driver.find_element(By.ID,"tags-container")
-                    print("Tags: ",tags.text)
+                    # print("Tags: ",tags.text)
                     Blog_dict["Tags"] = tags.text
                     
                     postdate = self.driver.find_element(By.CLASS_NAME,"entry-meta")
                     # print("postdate: ",postdate.text)
                     date = postdate.text
                     blogdate = date.split("\n")[0]
-                    print("Blogdate:",blogdate)
+                    # print("Blogdate:",blogdate)
                     Blog_dict["Blogdate"] = blogdate
                     
                     img_element = self.driver.find_element(By.XPATH,"//div[@class='post-thumbnail']/img")
                     img_url = img_element.get_attribute("src")
-                    print(img_url)
+                    # print(img_url)
                     Blog_dict["Image"] = img_url
                     
                     # Append this blog's data dictionary to the list
                     blogs_data.append(Blog_dict)
-                    print(blogs_data)
+                    # print(blogs_data)
                    
                     df = pd.DataFrame(blogs_data)
-                    print(df)
+                    # print(df)
                    
                     # Go back to the previous page
                     self.driver.back()
-                    sleep(10)
+                    # sleep(10)
                     
                     # Wait for the previous page to load
-                    WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "blog-card")))
+                    WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "blog-card")))
 
                 df.to_csv('IOTBlog.csv')
                 pagination = self.driver.find_element(By.CLASS_NAME,"pagination")
-                print(pagination.text)
+                # print(pagination.text)
                 
                 # locate the last pagination element
                 last_page_link = self.driver.find_element(By.CSS_SELECTOR,'ul.pagination li:last-child a')
-                print(last_page_link.text)
+                # print(last_page_link.text)
                 last_page_link.click()
-                sleep(10)
+                sleep(5)
                 if last_page_link:
                     ifpagination = True
                 else:
